73a2ce58141704dc1dd834b55360222c0c5d3040
ankit-infytrix <ankit.pandey@infytrix.com>
2026-01-20 14:50:06 +0530

    modified folder structure

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('requirements.txt') as f:
 
 setup(
     name='mark8_inventory_repo',
-    version='0.1.0',
+    version='0.2.0',
     description="Inventory integrations for multiple ecommerce/qcommerce platforms",
     author='Ankit Pandey',
     author_email='ankit.pandey@infytrix.com',
